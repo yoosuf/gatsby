4a58208839f4d1ce9eda67c9b2e6dd6f314b443b
Yoosuf Mo <mayoosuf@gmail.com>
2020-01-15 20:54:02 +0530

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  pathPrefix: "/gatsby",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Try Out`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -29,6 +30,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
